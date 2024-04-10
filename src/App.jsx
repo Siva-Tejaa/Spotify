5bb992bb0c2a6059edd31077ff509cf08b83f91a
Siva-Tejaa <asivateja1999@gmail.com>
2024-04-10 21:28:05 +0530

    local storage implemented for test login

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
 
   useEffect(() => {
     const isBrowserLogin = localStorage.getItem("isLoggedIn");
-    if (isBrowserLogin == true) {
+    if (JSON.parse(isBrowserLogin) == true) {
       dispatch(testLogin());
     }
   }, []);
